16595a56d9f80d8f9b9333b7a525befa06274419
Dylan <s1096777@student.hsleiden.nl>
2017-04-10 13:16:37 +0200

    HTTP request front end to back end

--- src/main/resources/public/app/services/TestService.js
+++ src/main/resources/public/app/services/TestService.js
@@ -1,3 +1,13 @@
 angular.module('waterscan').service('TestService', function ($http) {
+    var self = this;
 
+    self.getTest = function (onRetrieved) {
+        var url = '/test';
+        $http.get(url).then(function (response) {
+                onRetrieved(response.data);
+            },
+            function (response) {
+                console.log("Ophalen mislukt", response.data, "error");
+            });
+    };
 });
