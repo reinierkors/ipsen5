63e83e437e25219711f01c461790b96ea9abf51c
xDiglett <xdiglett@gmail.com>
2017-05-11 14:55:34 +0200

    Alle bestanden verplaatst, en branch gemaakt.

--- src/main/resources/public/app/controllers/TaxonController.js
+++
@@ -1,8 +0,0 @@
-angular.module('waterscan').controller('TaxonController', function ($scope, TaxonService) {
-    var self = this;
-
-    TaxonService.getAllTaxonData(function (data) {
-        var taxons = angular.fromJson(data);
-        console.log(taxons);
-    })
-});
