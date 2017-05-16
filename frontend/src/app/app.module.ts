e2ad73a7ad4b18f9196c6d9fe30c1148f7781517
xDiglett <xdiglett@gmail.com>
2017-05-16 13:13:43 +0200

    Create account & remove hash

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {Routes, RouterModule} from "@angular/router";
 import {MdCardModule} from '@angular/material';
+import {MdSidenavModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 const routes:Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'account', component: CreateAccountComponent},
   {path: '**', component: HomeComponent}
 ];
 
@@ -20,13 +23,15 @@ const routes:Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
+    CreateAccountComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MdCardModule,
-    RouterModule.forRoot(routes, {useHash: true}),
+    MdSidenavModule,
+    RouterModule.forRoot(routes, {useHash: false}),
   ],
   providers: [],
   bootstrap: [AppComponent]
