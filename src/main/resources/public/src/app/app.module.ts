a39e10fe92678a7276e7b68cfe574799becff9b6
xDiglett <xdiglett@gmail.com>
2017-05-16 10:46:19 +0200

    Trying to fix Material 4. Still broken

--- src/main/resources/public/src/app/app.module.ts
+++ src/main/resources/public/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import 'hammerjs';
+
+import { MaterialModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {Routes, RouterModule} from "@angular/router";
+import {MdCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MaterialModule } from '@angular/material';
-
 
 const routes:Routes = [
   {path: '', component: HomeComponent},
@@ -24,9 +25,10 @@ const routes:Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    MaterialModule,
     HttpModule,
+    MdCardModule,
     RouterModule.forRoot(routes, {useHash: true}),
-      MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
