c26fc9905082fbadc7ffbb3cc10f7f71c6fc8dac
Johan1403 <jokhan4@gmail.com>
2017-05-16 15:00:33 +0200

    sample table classes #2

---
+++ frontend/src/app/sample-location-table/sample-location-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-location-table',
+  templateUrl: './sample-location-table.component.html',
+  styleUrls: ['./sample-location-table.component.css']
+})
+export class SampleLocationTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
