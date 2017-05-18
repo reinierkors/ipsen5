07584777e29a4994822177fd8db3e813c15143d4
DylandeWit <dylandewitt@hotmail.com>
2017-05-18 15:38:04 +0200

    Map view v0.5 #1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,48 +1,51 @@
 import 'hammerjs';
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import {Routes, RouterModule} from "@angular/router";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {Routes, RouterModule} from '@angular/router';
 import {MdCardModule} from '@angular/material';
 import {MdMenuModule} from '@angular/material';
 import {MdToolbarModule} from '@angular/material';
 import {MdSidenavModule} from '@angular/material';
 import {MdListModule} from '@angular/material';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ResultsComponent } from './results/results.component';
-import { CreateAccountComponent } from './create-account/create-account.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {ResultsComponent} from './results/results.component';
+import {CreateAccountComponent} from './create-account/create-account.component';
+import {NguiMapModule} from '@ngui/map';
 
-const routes:Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'account', component: CreateAccountComponent},
-  {path: 'results', component: ResultsComponent},
-  {path: '**', component: HomeComponent}
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'account', component: CreateAccountComponent},
+    {path: 'results', component: ResultsComponent},
+    {path: '**', component: HomeComponent}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CreateAccountComponent,
-    ResultsComponent,
-  ],
-  imports: [
-    MdSidenavModule,
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    MdCardModule,
-    MdMenuModule,
-    MdListModule,
-    MdToolbarModule,
-    RouterModule.forRoot(routes, {useHash: false}),
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        CreateAccountComponent,
+        ResultsComponent,
+    ],
+    imports: [
+        MdSidenavModule,
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MdCardModule,
+        MdMenuModule,
+        MdListModule,
+        MdToolbarModule,
+        RouterModule.forRoot(routes, {useHash: false}),
+        NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyA8acwCuestYr1vo1mJK-QdkZ-3AwW1blM'})
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- frontend/src/app/results/results.component.ts
+++ frontend/src/app/results/results.component.ts
@@ -1,21 +1,29 @@
 import {Component, OnInit} from '@angular/core';
+import {NguiMapComponent} from '@ngui/map';
+
 
 @Component({
     selector: 'app-results',
     templateUrl: './results.component.html',
     styleUrls: ['./results.component.css']
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent {
     message = 'Message works!';
+    center = 'Leiden, The Netherlands';
+
+    positions = [
+        {lat: 51.63497, lng: 5.47588},
+        {lat: 51.50600, lng: 4.78167},
+        {lat: 53.03982, lng: 6.81579}
+    ];
+
+    onClick(event) {
+        if (event instanceof MouseEvent) {
+            return false;
+        }
+        console.log('map is clicked', event, event.target);
+    }
 
     constructor() {
-    };
-
-    printHello = function () {
-        console.log("hello")
-    };
-
-    ngOnInit() {
-        this.printHello()
     };
 }
