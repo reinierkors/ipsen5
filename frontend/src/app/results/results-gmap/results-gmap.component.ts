f50f3513812cf6c95d5d86e1f97bcfb4a7daaaf1
DylandeWit <dylandewitt@hotmail.com>
2017-05-26 15:15:17 +0200

    Styled the map for better water exposure and less clutter #1

--- frontend/src/app/results/results-gmap/results-gmap.component.ts
+++ frontend/src/app/results/results-gmap/results-gmap.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NguiMapComponent} from '@ngui/map';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute} from '@angular/router';
 import {ApiLocationService} from '../api.location.service';
 import {Location} from '../location.model';
 
@@ -14,82 +13,164 @@ export class ResultsComponent implements OnInit {
     private route: ActivatedRoute;
     private apiLocation: ApiLocationService;
     public locations: Location[];
+    public mapStyle = [
+        {
+            elementType: "labels",
+            stylers: [
+                {
+                    visibility: "off"
+                }
+            ]
+        },
+        {
+            featureType: "administrative.country",
+            stylers: [
+                {
+                    visibility: "on"
+                }
+            ]
+        },
+        {
+            featureType: "administrative.country",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#606060"
+                }
+            ]
+        },
+        {
+            featureType: "administrative.locality",
+            elementType: "labels",
+            stylers: [
+                {
+                    visibility: "simplified"
+                }
+            ]
+        },
+        {
+            featureType: "administrative.locality",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#606060"
+                }
+            ]
+        },
+        {
+            featureType: "road",
+            stylers: [
+                {
+                    visibility: "off"
+                }
+            ]
+        },
+        {
+            featureType: "water",
+            stylers: [
+                {
+                    visibility: "on"
+                }
+            ]
+        },
+        {
+            featureType: "water",
+            elementType: "geometry.fill",
+            stylers: [
+                {
+                    color: "#0eb9eb"
+                },
+                {
+                    visibility: "on"
+                }
+            ]
+        },
+        {
+            featureType: "water",
+            elementType: "labels",
+            stylers: [
+                {
+                    visibility: "on"
+                }
+            ]
+        },
+        {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#000000"
+                },
+                {
+                    visibility: "on"
+                }
+            ]
+        }
+    ];
+
+    public mapConfig = {
+        styles: this.mapStyle,
+        disableDefaultUI: true,
+        center: {lat: 52.152832, lng: 5.439478},
+        zoom: 8,
+    };
+
+    public marker = {
+        display: true,
+        id: 0,
+        code: '',
+        description: '',
+        lat: null,
+        lng: null,
+    };
+
+    positions = [];
 
     constructor(apiLocation: ApiLocationService, route: ActivatedRoute) {
         this.apiLocation = apiLocation;
         this.route = route;
-        this.initMarkers();
     };
 
     ngOnInit() {
         this.route.params
             .switchMap(params => this.apiLocation.getAllLocations())
-            .subscribe(markerLocations => {this.locations = markerLocations; console.log(this.locations)} , error => console.log(error));
+            .subscribe(markerLocations => {
+                this.locations = markerLocations;
+                this.initMarkers()
+            }, error => console.log(error));
     }
-
-    mapConfig = {
-        center: {lat: 52.152832, lng: 5.439478},
-        zoom: 8
-    };
-
-    marker = {
-        display: true,
-        id: "",
-        lat: null,
-        lng: null,
-        desc: "",
-    };
-
-    markers = [
-        {
-            display: true,
-            id: "mrk0",
-            lat: 51.63497,
-            lng: 5.47588,
-            desc: "Dit is water 1"
-        },
-        {
-            display: true,
-            id: "mrk1",
-            lat: 51.50600,
-            lng: 4.78167,
-            desc: "Dit is water 2"
-        },
-        {
-            display: true,
-            id: "mrk2",
-            lat: 53.03982,
-            lng: 6.81579,
-            desc: "Dit is water 3"
-        }
-    ];
-
-    positions = [];
 
     onClick(event) {
         const map = event.target;
-        console.log("markers", map.markers);
-        console.log("Lat:", event.latLng.lat());
-        console.log("Lng:", event.latLng.lng());
+        console.log('markers', map.markers);
+        console.log('Lat:', event.latLng.lat());
+        console.log('Lng:', event.latLng.lng());
     }
 
     showMarkerInfo({target: marker}) {
-        console.log("Le marker:", marker);
         this.retrieveMarker(marker);
         marker.nguiMapComponent.openInfoWindow('iw', marker);
     }
 
     retrieveMarker(marker) {
-        this.markers.forEach((item) => {
-            if (marker.getTitle() == item.id) {
-                this.marker = item;
+        this.locations.forEach((item) => {
+            if (marker.getTitle() == 'm' + <string><any>item.id) {
+                this.marker.id = item.id;
+                this.marker.code = item.code;
+                this.marker.description = item.description;
+                this.marker.lat = item.latitude;
+                this.marker.lng = item.longitude;
             }
         });
     }
 
     initMarkers() {
-        this.markers.forEach((item) => {
-            this.positions.push({lat: item.lat, lng: item.lng})
+        this.locations.forEach((item) => {
+            this.positions.push({
+                title: 'm' + <string><any>item.id,
+                lat: item.latitude,
+                lng: item.longitude
+            })
         });
     }
 }
