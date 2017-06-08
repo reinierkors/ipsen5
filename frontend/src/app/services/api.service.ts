94eb5360d141f43ce89bca9cfb646b44841acfe8
Wander <wander.gr@gmail.com>
2017-06-08 14:06:33 +0200

    getCookie in api service

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -56,4 +56,15 @@ export abstract class ApiService {
 				.subscribe(res=>this.transformResult(res,observer), err=>this.transformError(err,observer), ()=>observer.complete());
 		});
 	}
+	
+	protected getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		}
+		return "";
+	}
 }
