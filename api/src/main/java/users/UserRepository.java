8fae08dcbcbd838e83b916b7693382e687e602e7
Wander <wander.gr@gmail.com>
2017-06-10 18:00:53 +0200

    Don't reuse prepared statements across api calls

--- api/src/main/java/users/UserRepository.java
+++ api/src/main/java/users/UserRepository.java
@@ -10,32 +10,26 @@ import java.sql.*;
  * Repository for users
  *
  * @author Reinier
- * @version 0.1, 30-5-2017
+ * @version 0.2, 30-5-2017
  */
 public class UserRepository extends RepositoryMaria<User>{
-    private final PreparedStatement psFindByEmail;
-    private final PreparedStatement psFindBySession;
-    private final PreparedStatement psSaveSession;
-    private final PreparedStatement psDeleteSession;
-    private final PreparedStatement psEditPassword;
-	public UserRepository(Connection connection) {
+    private final String findByEmailQuery, findBySessionQuery, saveSessionQuery, deleteSessionQuery, editPasswordQuery;
+    
+    public UserRepository(Connection connection) {
 		super(connection);
-        String findByEmailQuery = "SELECT * FROM `"+getTable()+"` WHERE `email` LIKE ?";
-        String findBySessionQuery = "SELECT * FROM `"+getTable()+"` WHERE `session_token` = ?";
-        String saveSessionQuery = "UPDATE user SET session_token = ?, expiration_date = ? WHERE `id` = ?";
-        String deleteSessionQuery = "UPDATE user SET session_token = NULL, expiration_date = NULL WHERE `id` = ?";
-        String editPasswordQuery = "UPDATE user SET `password` = ? WHERE `session_token` = ?";
-        try {
-            psFindByEmail = connection.prepareStatement(findByEmailQuery);
-            psFindBySession = connection.prepareStatement(findBySessionQuery);
-            psSaveSession = connection.prepareStatement(saveSessionQuery);
-            psDeleteSession = connection.prepareStatement(deleteSessionQuery);
-            psEditPassword = connection.prepareStatement(editPasswordQuery);
-        } catch (SQLException e) {
-            throw new RepositoryException(e);
-        }
+        findByEmailQuery = "SELECT * FROM `"+getTable()+"` WHERE `email` LIKE ?";
+        findBySessionQuery = "SELECT * FROM `"+getTable()+"` WHERE `session_token` = ?";
+        saveSessionQuery = "UPDATE user SET session_token = ?, expiration_date = ? WHERE `id` = ?";
+        deleteSessionQuery = "UPDATE user SET session_token = NULL, expiration_date = NULL WHERE `id` = ?";
+        editPasswordQuery = "UPDATE user SET `password` = ? WHERE `session_token` = ?";
 	}
-
+	
+	private PreparedStatement psFindByEmail() throws SQLException {return connection.prepareStatement(findByEmailQuery);}
+	private PreparedStatement psFindBySession() throws SQLException {return connection.prepareStatement(findBySessionQuery);}
+	private PreparedStatement psSaveSession() throws SQLException {return connection.prepareStatement(saveSessionQuery);}
+	private PreparedStatement psDeleteSession() throws SQLException {return connection.prepareStatement(deleteSessionQuery);}
+	private PreparedStatement psEditPassword() throws SQLException {return connection.prepareStatement(editPasswordQuery);}
+	
 	@Override
 	protected String getTable() {
 		return "user";
@@ -71,13 +65,13 @@ public class UserRepository extends RepositoryMaria<User>{
 	 */
     public User findByEmail(String email){
         try {
+        	PreparedStatement psFindByEmail = psFindByEmail();
             psFindByEmail.setString(1, email);
             ResultSet resultSet = psFindByEmail.executeQuery();
-            if(resultSet.next()){
+            
+            if(resultSet!=null && resultSet.next())
                 return resultSetToModel(resultSet);
-            }
-            else
-                return null;
+            return null;
         } catch (SQLException e) {
             throw new RepositoryException(e);
         }
@@ -90,12 +84,13 @@ public class UserRepository extends RepositoryMaria<User>{
 	 */
 	public User findBySession(String sessionToken){
         try {
+        	PreparedStatement psFindBySession = psFindBySession();
             psFindBySession.setString(1, sessionToken);
             ResultSet resultSet = psFindBySession.executeQuery();
-            if(resultSet.next())
+            
+            if(resultSet!=null && resultSet.next())
                 return resultSetToModel(resultSet);
-            else
-                return null;
+            return null;
         } catch (SQLException e) {
             throw new RepositoryException(e);
         }
@@ -109,6 +104,7 @@ public class UserRepository extends RepositoryMaria<User>{
 	 */
 	public void saveSession(int id, String sessionToken, Timestamp expirationDate){
         try{
+        	PreparedStatement psSaveSession = psSaveSession();
             psSaveSession.setString(1, sessionToken);
             psSaveSession.setTimestamp(2, expirationDate);
             psSaveSession.setInt(3, id);
@@ -124,6 +120,7 @@ public class UserRepository extends RepositoryMaria<User>{
 	 */
 	public void deleteSession(int id){
         try{
+        	PreparedStatement psDeleteSession = psDeleteSession();
             psDeleteSession.setInt(1, id);
             psDeleteSession.executeUpdate();
         }catch (SQLException e) {
@@ -138,7 +135,8 @@ public class UserRepository extends RepositoryMaria<User>{
 	 */
 	public void editPassword(String newPassword, String sessionToken){
         try{
-            psEditPassword.setString(1, newPassword);
+            PreparedStatement psEditPassword = psEditPassword();
+        	psEditPassword.setString(1, newPassword);
             psEditPassword.setString(2, sessionToken);
             psEditPassword.executeUpdate();
         } catch (SQLException e) {
