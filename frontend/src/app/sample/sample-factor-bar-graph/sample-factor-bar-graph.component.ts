dbf53cb63b7dc28854044a07609b017a3557eef7
Wander <wander.gr@gmail.com>
2017-06-11 18:17:59 +0200

    Improvements and add reference to graph (using fake data atm)

--- frontend/src/app/sample/sample-factor-bar-graph/sample-factor-bar-graph.component.ts
+++ frontend/src/app/sample/sample-factor-bar-graph/sample-factor-bar-graph.component.ts
@@ -4,7 +4,10 @@ import {Sample} from '../sample.model';
 import {ApiSampleService,CalculationData} from '../api.sample.service';
 import {WEWFactor,WEWFactorClass} from '../../wew/wew.model';
 import {ApiWewService} from '../../wew/api.wew.service';
-import {MaterialPalette} from '../../services/palette';
+import {Palette,MaterialPalette} from '../../services/palette';
+
+//Value to put in the charts series.data list
+type DataValue = {factor:WEWFactor,factorClass:WEWFactorClass,value:number,itemStyle?:any,isReference?:boolean};
 
 
 @Component({
@@ -16,20 +19,22 @@ import {MaterialPalette} from '../../services/palette';
 export class SampleFactorBarGraphComponent implements OnInit {
 	//The sample this chart will show data of
 	@Input() sample:Sample;
+	//The reference for this sample
+	@Input() reference:null;
 	
-	//Only draw the chart once the data is in
-	public drawChart = false;
+	//EChart instance
+	private echart;
 	
 	//The colors used in this chart
-	private palette = new MaterialPalette();
+	private samplePalette:Palette = new MaterialPalette().shift();
+	private refPalette:Palette = new MaterialPalette().shift().transform(0,-.1,.35);
+	
 	//Promise that resolves when the wew factors are in
 	private getFactorsPr:Promise<WEWFactor[]>;
 	//Promise that resolves when the calculation data is in
-	private getCalcsPr:Promise<CalculationData[]>;
-	//List of all WEW factors
-	private factors:WEWFactor[];
-	//List of calculations for this sample
-	private calculations:CalculationData[];
+	private getSampleCalcsPr:Promise<CalculationData[]>;
+	//Promise that resolves when the calculation data is in
+	private getRefCalcsPr:Promise<CalculationData[]>;
 	
 	//EChart options
 	public chartOptions = {
@@ -37,14 +42,13 @@ export class SampleFactorBarGraphComponent implements OnInit {
 			text:'Watereigenschappen',
 			subtext:'Berekent met behulp van de WEW-lijst'
 		},
-		toolbox:{feature:{saveAsImage:{}}},
 		tooltip:{
 			trigger:'axis',
 			axisPointer:{type:'shadow'},
-			backgroundColor:'rgba(0,0,0,0.8)',
+			backgroundColor:'rgba(0,0,0,0.9)',
 			formatter:this.tooltipFormatter
 		},
-		legend:{data:[]},
+		//legend:{data:[]},
 		grid:{left:'3%',right:'4%',bottom:'3%',containLabel:true},
 		xAxis:{type:'category',data:[]},
 		yAxis:{type:'value',boundaryGap:[0, 0.01],min:0,max:10},
@@ -57,26 +61,38 @@ export class SampleFactorBarGraphComponent implements OnInit {
 	){
 		//Retrieve all factors
 		this.getFactorsPr = new Promise((resolve,reject) => this.wewApi.getFactors().subscribe(factors => {
-			this.factors = factors;
 			resolve(factors);
 		}));
 	}
 	
+	//@Input()s are available
 	ngOnInit(){
 		//Retrieve calculations for this sample
-		this.getCalcsPr = new Promise((resolve,reject) => this.sampleApi.getCalculationsBySample(this.sample.id).subscribe(calcs => resolve(calcs)));
+		this.getSampleCalcsPr = new Promise((resolve,reject) => this.sampleApi.getCalculationsBySample(this.sample.id).subscribe(calcs => resolve(calcs)));
+		
+		//Retrieve calculations for the reference
+		this.getRefCalcsPr = new Promise((resolve,reject) => resolve(null));
 		
 		//Then show the chart
-		Promise.all([this.getFactorsPr,this.getCalcsPr]).then(([factors,calcs]) => this.showData(factors,calcs));
+		Promise.all([this.getFactorsPr,this.getSampleCalcsPr,this.getRefCalcsPr])
+			.then(([factors,sampleCalcs,refCalcs]) => this.showData(factors,sampleCalcs,refCalcs));
+	}
+	
+	onChartInit(echart){
+		this.echart = echart;
+		echart.showLoading();
 	}
 	
 	//Returns the tooltip that is shown when hovering over the chart
 	private tooltipFormatter(params){
 		//If param.data is empty, then echart is passing on data from other bar stacks, ignore it
 		params = params.filter(param => param.data);
-		
 		if(!params.length)
 			return;
+		
+		//Find reference data for each
+		let refMap:Map<string/*factor class code*/,DataValue/*reference*/> = new Map();
+		params.map(p => p.data).filter(data => data.isReference).forEach((data:DataValue) => refMap.set(data.factorClass.code,data));
 		
 		//Show params in the same order as they appear in the chart
 		params.reverse();
@@ -85,60 +101,106 @@ export class SampleFactorBarGraphComponent implements OnInit {
 		return `<div class="tooltip-title">${params[0].data.factor.name}</div>
 			<p>
 				<table class="tooltip-table">
-					${params.map(p => `<tr>
+					<tr>
+						<th colspan="3">&nbsp;</th>
+						<th>Waarde</th>
+						<th>Referentie</th>
+					</tr>
+					${params.filter(p => !p.data.isReference).map(p => `<tr>
 						<td>${p.marker}</td>
 						<td>${p.data.factorClass.code}</td>
 						<td>${p.data.factorClass.description}</td>
 						<td>${p.data.value.toPrecision(3)}</td>
+						<td>${refMap.get(p.data.factorClass.code).value.toPrecision(3)}</td>
 					</tr>`).join('')}
 				</table>
 			</p>`;
 	}
 	
-	//Stores any data we need to show in this.chartOptions
-	private showData(factors:WEWFactor[],calcs:CalculationData[]){
+	//Make a 2D array with all the data (every row is factor, every column a factor class)
+	private dataAs2D(factors:WEWFactor[],calcs:CalculationData[]):DataValue[][]{
 		//We can have it as a map, it's from a single sample, so no duplicate factor classes
 		let calcMap = new Map<number/*factor class id*/,number/*computed value*/>();
 		calcs.forEach(calc => calcMap.set(calc.factorClassId,calc.computedValue));
 		
-		//Make sure 2 similar colors are not next to each other
-		this.palette.shift();
-		
+		//Fill the array
+		let data:DataValue[][] = [];
 		factors.forEach((factor,fIndex) => {
-			//Put factor names along the xAxis
-			this.chartOptions.xAxis.data.push(factor.name);
-			
-			//Make it so not all stacks have the same color order
-			this.palette.reverse();
-			this.palette.rotate(factor.classes.length);
-			
+			let dataRow:DataValue[] = [];
 			factor.classes.forEach((factorClass,fcIndex) => {
-				//Create the data object for the chart
-				let color = this.palette.colorAt(fcIndex).toString();
-				let series = {
-					name:factorClass.code,
-					type:'bar',
-					barGap:'-100%',
-					barCategoryGap:'30%',
-					stack:'stack-'+factor.id,
-					data:[
-						//This is because echarts maps the data index to the category index
-						//Without this, all bars are put under the first factor
-						...new Array(fIndex).fill(null),
-						{
-							//This data is available in the tooltipFormatter
-							factor:factor,
-							factorClass:factorClass,
-							value:calcMap.get(factorClass.id),
-							itemStyle:{normal:{color:color}}
-						}
-					]
-				};
-				
-				this.chartOptions.series.push(series);
+				dataRow.push({
+					factor:factor,
+					factorClass:factorClass,
+					value:calcMap.get(factorClass.id)
+				});
+			});
+			data.push(dataRow);
+		});
+		
+		//Make sure all rows have the same length (pad with null)
+		let max = data.map(row => row.length).reduce((next,max) => Math.max(next,max));
+		data = data.map(row => {
+			let len = row.length;
+			row.length = max;
+			row.fill(null,len);
+			return row;
+		});
+		
+		return data;
+	}
+	
+	//Stores any data we need to show in this.chartOptions
+	private showData(factors:WEWFactor[],sampleCalcs:CalculationData[],refCalcs:CalculationData[]){
+		//TODO we dont have ref calcs yet, so copy sample calcs
+		refCalcs = sampleCalcs;
+		
+		//Add x-axis labels
+		factors.forEach(factor => this.chartOptions.xAxis.data.push(factor.name));
+		
+		//Put values in a 2D array
+		let sampleData = this.dataAs2D(factors,sampleCalcs);
+		let refData = this.dataAs2D(factors,refCalcs);
+		
+		//Set property on ref data so we can tell them apart in the tooltip
+		refData.forEach(row => row.filter(val => !!val).forEach(val => val.isReference = true));
+		
+		//Add data
+		this.addSeries(refData,this.refPalette);
+		this.addSeries(sampleData,this.samplePalette);
+		
+		//These options should be applied to the last item in the series only
+		let lastSeries = this.chartOptions.series[this.chartOptions.series.length-1];
+		lastSeries.barGap = '10%';
+		lastSeries.barCategoryGap = '30%';
+		
+		//Show data
+		this.echart.setOption(this.chartOptions,true);
+		this.echart.hideLoading();
+	}
+	
+	private addSeries(data:DataValue[][],palette:Palette){
+		//Set chart colors
+		data.forEach(row => {
+			palette.reverse();
+			palette.rotate(3);
+			row.filter(val => val!==null).forEach((val,index) => {
+				val.itemStyle = {normal:{color:palette.colorAt(index).toString()}};
 			});
 		});
-		//Show the chart
-		this.drawChart = true;
+		
+		//Rotate the data array
+		let transpose = m => m[0].map((x,i) => m.map(x => x[i]))
+		data = transpose(data);
+		
+		//Put data in chart
+		let stack = Math.random();
+		data.forEach((row,index) => {
+			let series = {
+				type:'bar',
+				stack:stack,
+				data:row
+			};
+			this.chartOptions.series.push(series);
+		});
 	}
 }
--- frontend/src/app/services/palette.ts
+++ frontend/src/app/services/palette.ts
@@ -6,8 +6,8 @@ function hueList(count):number[]{
 	return hues;
 }
 
-//shiftEveryNth([1,2,3,4,5,6,7,8,9,10], 2) becomes [1,3,5,7,9,2,4,6,8,10]
-//shiftEveryNth([1,2,3,4,5,6,7,8,9,10], 3) becomes [1,4,7,10,2,5,8,3,6,9]
+//shiftParts([1,2,3,4,5,6,7,8,9,10], 2) becomes [1,3,5,7,9,2,4,6,8,10]
+//shiftParts([1,2,3,4,5,6,7,8,9,10], 3) becomes [1,4,7,10,2,5,8,3,6,9]
 function shiftParts<T>(arr:T[],shift:number):T[]{
 	let shiftedValues = [];
 	for(let i=0,count=arr.length,take=0,takeStart=0;i<count;++i){
@@ -28,27 +28,100 @@ function rotate<T>(arr:T[],rotateBy:number):T[]{
 	return [...arr.slice(rotateBy),...arr.slice(0,rotateBy)];
 }
 
+interface Color{
+	asHSL():HSL;
+	asRGB():RGB;
+	toString():string;
+}
+
 //Class representing an HSL color
-class HSL{
+//H, S and L are in range 0.0 .. 1.0
+class HSL implements Color{
 	public constructor(
 		public hue:number,
 		public saturation:number,
 		public lightness:number
 	){}
 	
+	//Copy of itself
+	public asHSL():HSL{
+		return new HSL(this.hue,this.saturation,this.lightness);
+	}
+	
+	//Convert to RGB
+	public asRGB():RGB{
+		let r,g,b;
+		if(this.saturation==0){
+			r = g = b = this.lightness;
+		}
+		else{
+			let hue2rgb = (p,q,t) => {
+				if(t<0)t += 1;
+				if(t>1)t -= 1;
+				if(t<1/6)return p + (q-p)*6*t;
+				if(t<1/2)return q;
+				if(t<2/3)return p + (q-p)*(2/3-t)*6;
+				return p;
+			};
+			
+			let q = this.lightness<.5 ? this.lightness*(1+this.saturation) : this.lightness+this.saturation-this.lightness*this.saturation;
+			let p = 2*this.lightness-q;
+			
+			r = hue2rgb(p,q,this.hue+1/3);
+			g = hue2rgb(p,q,this.hue);
+			b = hue2rgb(p,q,this.hue-1/3);
+		}
+		return new RGB(r*255,g*255,b*255);
+	}
+	
 	//Returns this color as hsl(h,s,l) string
 	public toString():string{
-		return `hsl(${this.hue},${this.saturation}%,${this.lightness}%)`;
+		return `hsl(${this.hue*360},${this.saturation*100}%,${this.lightness*100}%)`;
 	}
 }
 
 //Class representing an RGB color
-class RGB{
+//R, G and B are in range 0..255
+class RGB implements Color{
 	public constructor(
 		public red:number,
 		public green:number,
 		public blue:number
 	){}
+	
+	//Return copy of itself
+	public asRGB():RGB{
+		return new RGB(this.red,this.green,this.blue);
+	}
+	
+	//Convert to HSL
+	public asHSL():HSL{
+		let r = this.red/255;
+		let g = this.green/255;
+		let b = this.blue/255;
+		
+		let max = Math.max(r,g,b);
+		let min = Math.min(r,g,b);
+		let h,s,l = (max+min)/2;
+
+		if(max===min){
+			h = s = 0;
+		}
+		else {
+			let d = max-min;
+			s = l>.5 ? d/(2-max-min) : d/(max+min);
+			
+			switch(max){
+				case r: h = (g-b)/d + (g<b?6:0);break;
+				case g: h = (b-r)/d + 2;break;
+				case b: h = (r-g)/d + 4;break;
+			}
+			
+			h /= 6;
+		}
+		
+		return new HSL(h,s,l);
+	}
 	
 	//Returns this color as rgb(r,g,b) string
 	public toString():string{
@@ -57,46 +130,57 @@ class RGB{
 }
 
 //A palette is a list of HSL or RGB colors
-abstract class Palette{
-	protected colors:(HSL|RGB)[];
-	protected loop:boolean;
+export abstract class Palette{
+	public colors:Color[];
+	public loop:boolean;
 	
 	public constructor(loop?:boolean){
 		this.loop = loop==null?true:loop;
 	}
 	
-	//Returns all colors in this palette
-	public getColors():(HSL|RGB)[]{
-		return this.colors;
-	}
-	
 	//When loop is off, index must be smaller than colors.length
-	public colorAt(index:number):(HSL|RGB){
+	public colorAt(index:number):Color{
 		return this.loop?this.colors[index%this.colors.length]:this.colors[index];
 	}
 	
 	//Rotate the color palette
-	public rotate(amount?:number):void{
+	public rotate(amount?:number):Palette{
 		if(!amount)
 			amount = Math.floor(Math.random()*this.colors.length)
 		this.colors = rotate(this.colors,amount);
+		return this;
 	}
 	
 	//Shifts values within the palette apart from each other
-	public shift(amount?:number):void{
+	public shift(amount?:number):Palette{
 		if(!amount)
 			amount = Math.round(this.colors.length**.5)+1
 		this.colors = shiftParts(this.colors,amount);
+		return this;
 	}
 	
 	//Reverses the order of colors in the palete
-	public reverse(){
+	public reverse():Palette{
 		this.colors.reverse();
+		return this;
 	}
 	
 	//Randomizes the order of the palette
-	public randomize(){
+	public randomize():Palette{
 		shuffleArray(this.colors);
+		return this;
+	}
+	
+	//Changes every color in the filter
+	public transform(hue:number,saturation:number,lightness:number):Palette{
+		this.colors = this.colors.map(color => {
+			let c = color.asHSL();
+			c.hue = Math.max(Math.min(c.hue+hue,1),0);
+			c.saturation = Math.max(Math.min(c.saturation+saturation,1),0);
+			c.lightness = Math.max(Math.min(c.lightness+lightness,1),0);
+			return c;
+		});
+		return this;
 	}
 }
 
@@ -106,7 +190,7 @@ export class RandomPalette extends Palette{
 		super(false);
 		this.colors = [];
 	}
-	public colorAt(index?:number):HSL{
+	public colorAt(index?:number):Color{
 		return new HSL(Math.floor(Math.random()*360),Math.round(Math.random()*100),Math.round(Math.random()*100));
 	}
 }
@@ -125,28 +209,28 @@ export class HuePalette extends Palette{
 //A palette of pastel colors
 export class PastelPalette extends HuePalette{
 	public constructor(howManyColors:number,loop?:boolean){
-		super(howManyColors,100,80,loop);
+		super(howManyColors,1,.8,loop);
 	}
 }
 
 //Dark colors
 export class DarkPalette extends HuePalette{
 	public constructor(howManyColors:number,loop?:boolean){
-		super(howManyColors,100,20,loop);
+		super(howManyColors,1,.2,loop);
 	}
 }
 
 //Colors with saturation and lightness at 50%
 export class PlainPalette extends HuePalette{
 	public constructor(howManyColors:number,loop?:boolean){
-		super(howManyColors,50,50,loop);
+		super(howManyColors,.5,.5,loop);
 	}
 }
 
 //Bright colors
 export class BrightPalette extends HuePalette{
 	public constructor(howManyColors:number,loop?:boolean){
-		super(howManyColors,100,50,loop);
+		super(howManyColors,1,.5,loop);
 	}
 }
 
