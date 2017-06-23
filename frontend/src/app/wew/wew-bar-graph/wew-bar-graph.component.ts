239991084c240e1e7b5d09a76e99344285da2a52
Wander <wander.gr@gmail.com>
2017-06-23 15:23:28 +0200

    Better support for multiple samples/references

--- frontend/src/app/wew/wew-bar-graph/wew-bar-graph.component.ts
+++ frontend/src/app/wew/wew-bar-graph/wew-bar-graph.component.ts
@@ -11,14 +11,14 @@ import {Palette,MaterialPalette} from '../../services/palette';
 import 'rxjs/add/operator/toPromise';
 
 //Value to put in the charts series.data list
-type DataValue = {factor:WEWFactor,factorClass:WEWFactorClass,value:number,itemStyle?:any,entity:Reference|Sample,name:string};
+type DataValue = {factorId:number,factorClassId:number,value:number,itemStyle?:any,entityId:number,entityType:'sample'|'reference',name:string};
 
 
 @Component({
-    selector: 'app-wew-bar-graph',
-    providers: [ApiSampleService,ApiWewService,ApiReferenceService],
-    templateUrl: './wew-bar-graph.component.html',
-    styleUrls: ['./wew-bar-graph.component.css']
+	selector:'app-wew-bar-graph',
+	providers:[ApiSampleService,ApiWewService,ApiReferenceService],
+	templateUrl:'./wew-bar-graph.component.html',
+	styleUrls:['./wew-bar-graph.component.css']
 })
 export class WewBarGraphComponent implements OnInit {
 	//The samples this chart will show data of
@@ -28,44 +28,43 @@ export class WewBarGraphComponent implements OnInit {
 	//The factors to show
 	@Input('factors') inputFactors:WEWFactor[];
 	
-	
-	//temp
-	testSettings = {h:0,s:-.1,l:.3,o:30};
-	testPresets = [
-		{name:'Dark',h:-.02,s:.15,l:-.22,o:null},
-		{name:'Light',h:.02,s:-.1,l:.3,o:null},
-		{name:'Overlap',h:null,s:null,l:null,o:-66},
-		{name:'Seperated',h:null,s:null,l:null,o:30}
-	];
-	testBtn(preset){
-		if(preset.h!==null)this.testSettings.h = preset.h;
-		if(preset.s!==null)this.testSettings.s = preset.s;
-		if(preset.l!==null)this.testSettings.l = preset.l;
-		if(preset.o!==null)this.testSettings.o = preset.o;
-		this.testUpdate();
-	}
-	testUpdate(){
-		this.refPalette = new MaterialPalette().shift().transform(this.testSettings.h,this.testSettings.s,this.testSettings.l);
-		this.samplePalette = new MaterialPalette().shift();
-		//Promise.all([this.getFactorsPr,this.getSampleCalcsPr,this.getRefCalcsPr])
-			//.then(([factors,sampleCalcs,refCalcs]) => this.showData(factors,sampleCalcs,refCalcs));
-	}
-	
-	//EChart instance
-	private echart;
-	
+	/* SETTINGS: SIZE */
+	//Size of the graph in pixels
+	public width:number;
+	public height:number;
+	//Space between bars within the same category
+	private barGap:string = '10%';
+	//Space between categories
+	private barCategoryGap:string = '30%';
+	/* SETTINGS: COLOR */
 	//The colors used in this chart
 	private samplePalette:Palette = new MaterialPalette().shift();
 	private refPalette:Palette = new MaterialPalette().shift().transform(0,-.1,.3);
 	
+	
+	//EChart instance
+	private echart;
 	//Promise that resolves when the wew factors are in
 	private getFactorsPr:Promise<WEWFactor[]>;
 	//Promise that resoles when all required data is loaded
-	private dataPr:Promise<null>;
+	private allDataPr:Promise<[WEWFactor[],Map<Sample,CalculationData[]>,Map<Reference,CalculationData[]>]>;
+	
+	/* Maps and arrays for quick lookup and looping */
+	private sampleCalcs:Map<Sample,CalculationData[]>;
+	private referenceCalcs:Map<Reference,CalculationData[]>;
+	private references:Reference[];
+	private samples:Sample[];
+	private factors:WEWFactor[];
+	private factorClasses:WEWFactorClass[];
+	private factorIdMap:Map<number/*factor id*/,WEWFactor>;
+	private factorClassIdMap:Map<number/*factor class id*/,WEWFactorClass>;
+	private referenceIdMap:Map<number/*reference id*/,Reference>;
+	private sampleIdMap:Map<number/*sample id*/,Sample>;
+	
 	
 	//EChart options
 	public chartOptions = {
-		title: {
+		title:{
 			text:'Watereigenschappen',
 			subtext:'Berekent met behulp van de WEW-lijst'
 		},
@@ -73,7 +72,7 @@ export class WewBarGraphComponent implements OnInit {
 			trigger:'axis',
 			axisPointer:{type:'shadow'},
 			backgroundColor:'rgba(0,0,0,0.9)',
-			formatter:this.tooltipFormatter
+			formatter:(p)=>this.tooltipFormatter(p)
 		},
 		//legend:{data:[]},
 		grid:{left:'3%',right:'4%',bottom:'3%',containLabel:true},
@@ -100,14 +99,52 @@ export class WewBarGraphComponent implements OnInit {
 		let sampleCalcsPr = this.loadSampleCalculations();
 		let referenceCalcsPr = this.loadReferenceCalculations();
 		
+		//Is all data we need loaded?
+		this.allDataPr = Promise.all([this.getFactorsPr,sampleCalcsPr,referenceCalcsPr]);
+		//Fill Maps
+		this.allDataPr.then(([factors,sampleCalcs,refCalcs]) => this.storeCollections(factors,sampleCalcs,refCalcs));
+		//Calculate Size
+		this.allDataPr.then(() => this.calculateSize());
 		//Then show the chart
-		Promise.all([this.getFactorsPr,sampleCalcsPr,referenceCalcsPr])
-			.then(([factors,sampleCalcs,refCalcs]) => this.showData(factors,sampleCalcs,refCalcs));
+		this.allDataPr.then(() => this.showData());
 	}
 	
-	onChartInit(echart){
+	//EChart instance is available
+	public onChartInit(echart){
 		this.echart = echart;
 		echart.showLoading();
+	}
+	
+	private calculateSize(){
+		this.width = 1000;
+		this.height = 300;
+		this.echart.resize({width:this.width,height:this.height});
+	}
+	
+	//Put data in arrays and maps for easy access
+	private storeCollections(factors:WEWFactor[],sampleCalcs:Map<Sample,CalculationData[]>,referenceCalcs:Map<Reference,CalculationData[]>):void{ console.log(factors);
+		this.sampleCalcs = sampleCalcs;
+		this.referenceCalcs = referenceCalcs;
+		this.references = Array.from(referenceCalcs.keys());
+		this.samples = Array.from(sampleCalcs.keys());
+		this.factors = factors;
+		this.factorClasses = factors.reduce((arr,f) => [...arr,...f.classes],[]);
+		
+		//Map<number/*factor id*/,WEWFactor>
+		this.factorIdMap = new Map();
+		this.factors.forEach(f => this.factorIdMap.set(f.id,f));
+		
+		//Map<number/*factor class id*/,WEWFactorClass>
+		this.factorClassIdMap = new Map();
+		this.factorClasses.forEach(fc => this.factorClassIdMap.set(fc.id,fc));
+		
+		//Map<number/*reference id*/,Reference>
+		this.referenceIdMap = new Map();
+		this.references.forEach(ref => this.referenceIdMap.set(ref.id,ref));
+		
+		//Map<number/*sample id*/,Sample>
+		this.sampleIdMap = new Map();
+		this.samples.forEach(sample => this.sampleIdMap.set(sample.id,sample));
 	}
 	
 	//Loads the calculations for all samples
@@ -138,96 +175,24 @@ export class WewBarGraphComponent implements OnInit {
 		return Promise.all(referencePromises).then(() => referenceCalcs);
 	}
 	
-	//Returns the tooltip that is shown when hovering over the chart
-	//ToDo: do less in this method to speed things up
-	private tooltipFormatter(params){
-		//If param.data is empty, then echart is passing on data from other bar stacks, ignore it
-		params = params.filter(param => param.data);
-		if(!params.length)
-			return;
-		
-		//Find factor classes
-		let factorClassMap:Map<string/*code*/,WEWFactorClass> = new Map();
-		params.map(p => p.data.factorClass).forEach(fc => factorClassMap.set(fc.code,fc));
-		let factorClasses = Array.from(factorClassMap.values()).sort((fc1,fc2) => fc2.order-fc1.order);
-		
-		//Prevent duplicate samples and references
-		let refIdMap:Map<number/*ref id*/,Reference> = new Map();
-		let sampleIdMap:Map<number/*sample id*/,Sample> = new Map();
-		params.map(p => p.data).forEach((data:DataValue) => {
-			let idMap = data.entity instanceof Sample?sampleIdMap:refIdMap;
-			let entity = idMap.get(data.entity.id);
-			if(entity)
-				data.entity = entity;
-			else
-				(<any>idMap).set(data.entity.id,data.entity);
-		});
-		
-		//Find references, samples and their data
-		let dataMap:Map<Sample|Reference, Map<WEWFactorClass,DataValue>> = new Map();
-		let nameMap:Map<Reference|Sample,string/*name*/> = new Map();
-		
-		params.map(p => p.data).forEach((data:DataValue) => {
-			//let dataMap = data.entity instanceof Sample?sampleDataMap:refDataMap;
-		 	let map = dataMap.get(data.entity);
-			if(!map){
-				map = new Map();
-				dataMap.set(data.entity,map);
-			}
-			map.set(factorClassMap.get(data.factorClass.code),data);
-			nameMap.set(data.entity,data.name);
-		});
-		
-		let entities:(Sample|Reference)[] = Array.from(dataMap.keys());
-		let samples:Sample[] = <Sample[]>entities.filter(entity => entity instanceof Sample);
-		let references:Reference[] = <Reference[]>entities.filter(entity => entity instanceof Reference);
-		let names:string[] =  Array.from(nameMap.values());
-		
-		//Show sample colors if there are any samples, otherwise use reference colors
-		let markers:Map<WEWFactorClass,string/*marker html*/> = new Map();
-		if(samples.length)
-			params.filter(p => p.data.sample).forEach(p => markers.set(p.data.factorClass,p.marker));
-		else
-			params.forEach(p => markers.set(p.data.factorClass,p.marker));
-		
-		//Return html to be put in the tooltip
-		return `<div class="tooltip-title">${params[0].data.factor.name}</div>
-			<p>
-				<table class="tooltip-table">
-					<tr>
-						<th colspan="3">&nbsp;</th>
-						${names.map(name => `<th>${name}</th>`).join('')}
-					</tr>
-					${factorClasses.map(fc => `<tr>
-						<td>${markers.get(fc)}</td>
-						<td>${fc.code}</td>
-						<td>${fc.description}</td>
-						${entities.map(entity => {
-							let value = dataMap.get(entity).get(fc).value;
-							return `<td>${value===null?'?':value.toFixed(2)}</td>`
-						}).join('')}
-					</tr>`).join('')}
-				</table>
-			</p>`;
-	}
-	
 	//Make a 2D array with all the data (every row is factor, every column a factor class)
-	private dataAs2D(factors:WEWFactor[],calcs:CalculationData[],entity:Sample|Reference,name:string):DataValue[][]{
+	private dataAs2D(calcs:CalculationData[],entity:Sample|Reference,name:string):DataValue[][]{
 		//We can have it as a map, it's from a single sample, so no duplicate factor classes
 		let calcMap = new Map<number/*factor class id*/,number/*computed value*/>();
 		calcs.forEach(calc => calcMap.set(calc.factorClassId,calc.computedValue));
 		
 		//Fill the array
 		let data:DataValue[][] = [];
-		factors.forEach((factor,fIndex) => {
+		this.factors.forEach((factor,fIndex) => {
 			let dataRow:DataValue[] = [];
 			factor.classes.forEach((factorClass,fcIndex) => {
-				let valueObj = {
-					factor:factor,
-					factorClass:factorClass,
+				let valueObj:DataValue = {
+					factorId:factor.id,
+					factorClassId:factorClass.id,
 					value:calcMap.get(factorClass.id),
 					name:name,
-					entity:entity
+					entityId:entity.id,
+					entityType:entity instanceof Sample?'sample':'reference'
 				};
 				dataRow.push(valueObj);
 			});
@@ -247,30 +212,30 @@ export class WewBarGraphComponent implements OnInit {
 	}
 	
 	//Stores any data we need to show in this.chartOptions
-	private showData(factors:WEWFactor[],sampleCalcs:Map<Sample,CalculationData[]>,refCalcs:Map<Reference,CalculationData[]>){
+	private showData(){
 		//Add x-axis labels
 		this.chartOptions.xAxis.data = [];
-		factors.forEach(factor => this.chartOptions.xAxis.data.push(factor.name));
+		this.factors.forEach(factor => this.chartOptions.xAxis.data.push(factor.name));
 		
 		//Clear any previous data
 		this.chartOptions.series = [];
 		
 		//Add reference data
-		refCalcs.forEach((calcs,reference) => {
-			let refData = this.dataAs2D(factors,calcs,reference,'Referentie');
+		this.referenceCalcs.forEach((calcs,reference) => {
+			let refData = this.dataAs2D(calcs,reference,'Referentie');
 			this.addSeries(refData,this.refPalette);
 		});
 		
 		//Add sample data
-		sampleCalcs.forEach((calcs,sample) => {
-			let sampleData = this.dataAs2D(factors,calcs,sample,'Monster');
+		this.sampleCalcs.forEach((calcs,sample) => {
+			let sampleData = this.dataAs2D(calcs,sample,'Monster');
 			this.addSeries(sampleData,this.samplePalette);
 		});
 		
 		//These options should be applied to the last item in the series only
 		let lastSeries = this.chartOptions.series[this.chartOptions.series.length-1];
-		lastSeries.barGap = this.testSettings.o+'%';
-		lastSeries.barCategoryGap = '30%';
+		lastSeries.barGap = this.barGap;
+		lastSeries.barCategoryGap = this.barCategoryGap;
 		
 		//Show data
 		this.echart.setOption(this.chartOptions,true);
@@ -279,13 +244,16 @@ export class WewBarGraphComponent implements OnInit {
 	
 	private addSeries(data:DataValue[][],palette:Palette){
 		//Set chart colors
-		data.forEach((row) => {
+		data.forEach(row => {
 			palette.reverse();
 			palette.rotate(3);
 			row.filter(val => val!==null).forEach((val,index) => {
 				val.itemStyle = {normal:{color:palette.colorAt(index).toString()}};
 			});
 		});
+		
+		//Reverse the rows, so the first objects show at the top of the graph instead of the bottom
+		data.forEach(row => row.reverse());
 		
 		//Rotate the data array
 		let transpose = m => m[0].map((x,i) => m.map(x => x[i]));
@@ -301,5 +269,61 @@ export class WewBarGraphComponent implements OnInit {
 			};
 			this.chartOptions.series.push(series);
 		});
+	}
+	
+	//Returns the tooltip that is shown when hovering over the chart
+	private tooltipFormatter(params){
+		//If param.data is empty, then echart is passing on data from other bar stacks, ignore it
+		params = params.filter(param => param.data);
+		if(!params.length)
+			return;
+		
+		//The factor we're hovering over
+		let factor:WEWFactor = this.factorIdMap.get(params[0].data.factorId);
+		
+		//Find references, samples and their data
+		let dataMap:Map<Sample|Reference, Map<WEWFactorClass,DataValue>> = new Map();
+		let nameMap:Map<Reference|Sample,string/*name*/> = new Map();
+		
+		params.map(p => p.data).forEach((data:DataValue) => {
+			let entity = data.entityType==='sample'?this.sampleIdMap.get(data.entityId):this.referenceIdMap.get(data.entityId);
+			let map = dataMap.get(entity);
+			if(!map){
+				map = new Map();
+				dataMap.set(entity,map);
+			}
+			map.set(this.factorClassIdMap.get(data.factorClassId),data);
+			nameMap.set(entity,data.name);
+		});
+		
+		let names:string[] =  Array.from(nameMap.values());
+		
+		//Show sample colors if there are any samples, otherwise use reference colors
+		let markers:Map<WEWFactorClass,string/*marker html*/> = new Map();
+		let markerParams = params.filter(p => p.data.entityType==='sample');
+		if(!markerParams.length)
+			markerParams = params;
+		markerParams.forEach(p => markers.set(this.factorClassIdMap.get(p.data.factorClassId),p.marker));
+		
+		//Return html to be put in the tooltip
+		return `<div class="tooltip-title">${factor.name}</div>
+			<p>
+				<table class="tooltip-table">
+					<tr>
+						<th colspan="3">&nbsp;</th>
+						${names.map(name => `<th>${name}</th>`).join('')}
+					</tr>
+					${factor.classes.map(fc => `<tr>
+						<td>${markers.get(fc)}</td>
+						<td>${fc.code}</td>
+						<td>${fc.description}</td>
+						${[...this.references,...this.samples].map(entity => {
+							let dataValue = dataMap.get(entity).get(fc);
+							let value = dataValue?dataValue.value:0;
+							return `<td>${value.toFixed(2)}</td>`
+						}).join('')}
+					</tr>`).join('')}
+				</table>
+			</p>`;
 	}
 }
